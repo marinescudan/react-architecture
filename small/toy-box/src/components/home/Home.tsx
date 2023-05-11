54ee134aeed4b4d7535163fade2dbd42457befe4
Dan Marinescu <marinescudan@users.noreply.github.com>
2023-05-11 13:53:41 +0000

    Adds styled button component

--- small/toy-box/src/components/home/Home.tsx
+++ small/toy-box/src/components/home/Home.tsx
@@ -1,6 +1,6 @@
 
 import React from 'react';
-
+import Hero from '../shared/Hero';
 import './Home.scss';
 
 export interface IHomeProps {}
--- small/toy-box/src/components/shared/Button.tsx
+++ small/toy-box/src/components/shared/Button.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import styled from 'styled-components';
 
-export interface IButtonProps {}
+export interface IButtonProps {
+  onClick?: () => void;
+  text?: string;
+  bg?: string;
+}
 
-const Button = ({}: IButtonProps) => {
-  return <div>Hello 👋, I am a Button component.</div>;
+const StyledButton = styled.button`
+  background-color: ${ (props:IButtonProps) => props.bg? props.bg : "white"};
+  font-size: 32px;
+  color: white;
+`;
+
+const Button = ({
+  onClick,
+  text,
+}: IButtonProps) => {
+  return <StyledButton> {text} </StyledButton>;
 };
 
 export default Button;
