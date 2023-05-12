50d0bec85e9f17914d890e4d231b47aff792ec84
Dan Marinescu <marinescudan@users.noreply.github.com>
2023-05-12 15:36:47 +0000

    Adds functionality to Home component

--- small/toy-box/src/components/home/Home.tsx
+++ small/toy-box/src/components/home/Home.tsx
@@ -30,38 +30,67 @@ const Home = ({}: IHomeProps) => {
                 description={heroProps.description}
                 cta={heroProps.cta}
             ></Hero>
-            <section className='product-list'>
-                <div className='product-card'>
-                    <h3>Product 1</h3>
-                    <p>$1.00</p>
-                    <button>Add to Cart</button>
-                </div>
-                <div className='product-card'>
-                    <h3>Product 2</h3>
-                    <p>$2.00</p>
-                    <button>Add to Cart</button>
-                </div>
-                <div className='product-card'>
-                    <h3>Product 3</h3>
-                    <p>$3.00</p>
-                    <button>Add to Cart</button>
-                </div>
-                <div className='product-card'>
-                    <h3>Product 4</h3>
-                    <p>$4.00</p>
-                    <button>Add to Cart</button>
-                </div>
-                <div className='product-card'>
-                    <h3>Product 5</h3>
-                    <p>$5.00</p>
-                    <button>Add to Cart</button>
-                </div>
-                <div className='product-card'>
-                    <h3>Product 6</h3>
-                    <p>$6.00</p>
-                    <button>Add to Cart</button>
-                </div>
-            </section>
+            <main>
+                <section>
+                    <h1 className='centered'>New commers</h1>
+                </section>
+                <section className='product-list'>
+                    <div className='product-card'>
+                        <figure>
+                            <img src='https://fastly.picsum.photos/id/96/4752/3168.jpg?hmac=KNXudB1q84CHl2opIFEY4ph12da5JD5GzKzH5SeuRVM' alt='Product 1' />
+                        </figure>
+                        <h3>Product 1</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio earum, enim nostrum distinctio odio inventore exercitationem soluta non explicabo consequuntur quo, dolores vero esse, fuga beatae praesentium architecto eligendi assumenda!</p>
+                        <p>$1.00</p>
+                        <button>Add to Cart</button>
+                    </div>
+                    <div className='product-card'>
+                        <figure>
+                            <img src='https://fastly.picsum.photos/id/96/4752/3168.jpg?hmac=KNXudB1q84CHl2opIFEY4ph12da5JD5GzKzH5SeuRVM' alt='Product 1' />
+                        </figure>
+                        <h3>Product 2</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio earum, enim nostrum distinctio odio inventore exercitationem soluta non explicabo consequuntur quo, dolores vero esse, fuga beatae praesentium architecto eligendi assumenda!</p>
+                        <p>$2.00</p>
+                        <button>Add to Cart</button>
+                    </div>
+                    <div className='product-card'>
+                        <figure>
+                            <img src='https://fastly.picsum.photos/id/96/4752/3168.jpg?hmac=KNXudB1q84CHl2opIFEY4ph12da5JD5GzKzH5SeuRVM' alt='Product 1' />
+                        </figure>
+                        <h3>Product 3</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio earum, enim nostrum distinctio odio inventore exercitationem soluta non explicabo consequuntur quo, dolores vero esse, fuga beatae praesentium architecto eligendi assumenda!</p>
+                        <p>$3.00</p>
+                        <button>Add to Cart</button>
+                    </div>
+                    <div className='product-card'>
+                        <figure>
+                            <img src='https://fastly.picsum.photos/id/96/4752/3168.jpg?hmac=KNXudB1q84CHl2opIFEY4ph12da5JD5GzKzH5SeuRVM' alt='Product 1' />
+                        </figure>
+                        <h3>Product 4</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio earum, enim nostrum distinctio odio inventore exercitationem soluta non explicabo consequuntur quo, dolores vero esse, fuga beatae praesentium architecto eligendi assumenda!</p>
+                        <p>$4.00</p>
+                        <button>Add to Cart</button>
+                    </div>
+                    <div className='product-card'>
+                        <figure>
+                            <img src='https://fastly.picsum.photos/id/96/4752/3168.jpg?hmac=KNXudB1q84CHl2opIFEY4ph12da5JD5GzKzH5SeuRVM' alt='Product 1' />
+                        </figure>
+                        <h3>Product 5</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio earum, enim nostrum distinctio odio inventore exercitationem soluta non explicabo consequuntur quo, dolores vero esse, fuga beatae praesentium architecto eligendi assumenda!</p>
+                        <p>$5.00</p>
+                        <button>Add to Cart</button>
+                    </div>
+                    <div className='product-card'>
+                        <figure>
+                            <img src='https://fastly.picsum.photos/id/96/4752/3168.jpg?hmac=KNXudB1q84CHl2opIFEY4ph12da5JD5GzKzH5SeuRVM' alt='Product 1' />
+                        </figure>
+                        <h3>Product 6</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio earum, enim nostrum distinctio odio inventore exercitationem soluta non explicabo consequuntur quo, dolores vero esse, fuga beatae praesentium architecto eligendi assumenda!</p>
+                        <p>$6.00</p>
+                        <button>Add to Cart</button>
+                    </div>
+                </section>
+            </main>
         </div>
     );
 };
--- small/toy-box/src/components/shared/Footer.tsx
+++ small/toy-box/src/components/shared/Footer.tsx
@@ -6,9 +6,6 @@ const Footer = ({}: IFooterProps) => {
   return (
     <footer>
       <div className='column'>
-          <img src="logo192.png" alt="logo" />
-      </div>
-      <div className='column'>
           <ul>
             <li>
               <Link to="/">Home</Link>
@@ -19,13 +16,6 @@ const Footer = ({}: IFooterProps) => {
             <li>
               <Link to="/cart">Cart</Link>
             </li>
-          </ul>
-      </div>
-      <div className='column'>
-          <ul>
-              <li>Facebook</li>
-              <li>Twitter</li>
-              <li>Instagram</li>
           </ul>
       </div>
     </footer>
